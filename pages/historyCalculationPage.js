9a565b276d320a6671bfcb55a7afa399caacdbed
satnodeus77 <satnodeus77@users.noreply.github.com>
2025-02-15 08:35:22 +0700

    Added automatic database table creation in db.js

--- pages/historyCalculationPage.js
+++ pages/historyCalculationPage.js
@@ -176,7 +176,7 @@ export default function HistoryCalculationPage() {
                     <TableCell>
                       {calculation.rank_results.map((result, index) => (
                         <div key={index}>
-                          <strong>{result.name}</strong>: Rank {result.rank}, Score {result.score}
+                          <strong>{result.name}</strong>, Score {result.score}
                         </div>
                       ))}
                     </TableCell>
